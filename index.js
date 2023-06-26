857464d754527e555948d39207478039e92cd758
Petrochenk0 <96386665+Petrochenk0@users.noreply.github.com>
2023-06-26 20:56:12 +0300

    experience
    experience

---
+++ index.js
@@ -0,0 +1,11 @@
+// Дополнительные действия при наведении можно добавить с помощью JavaScript
+var zoomContainer = document.querySelector('.zoom-container');
+var image = zoomContainer.querySelector('img');
+
+zoomContainer.addEventListener('mouseenter', function () {
+  // Дополнительные действия при наведении
+});
+
+zoomContainer.addEventListener('mouseleave', function () {
+  // Дополнительные действия при покидании элемента
+});
